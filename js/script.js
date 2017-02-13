5aa8d6be2d338164ae8c19dedb0d606356f172e2
AlexanderBrenchev <alexzund3r@yahoo.com>
2017-02-13 22:23:04 +0700

    Fixes and animation for write us modal

--- js/script.js
+++ js/script.js
@@ -1,12 +1,15 @@
 
 var purchaseBtns = document.querySelectorAll('.buy-btn'),
     modal = document.querySelector('.modal-window'),
-    closeModalBtns = document.querySelectorAll('.close-btn');
+    closeModalBtns = document.querySelectorAll('.close-btn'),
+    writeUs = document.querySelector('.contacts .btn');
 
 function toggleModal(evt) {
   evt.preventDefault();
   modal.classList.toggle('opened');
 }
+
+if (writeUs) writeUs.addEventListener('click', toggleModal);
 
 purchaseBtns.forEach(function(btn) {
   btn.addEventListener('click', toggleModal);
