8ec8469650996dd63eaf888d7296ef954c2f1956
AlexanderBrenchev <alexzund3r@yahoo.com>
2017-02-14 20:47:00 +0700

    forEach fix, service tab activated, link from catalog page to index and vice-versa

--- js/script.js
+++ js/script.js
@@ -2,19 +2,39 @@
 var purchaseBtns = document.querySelectorAll('.buy-btn'),
     modal = document.querySelector('.modal-window'),
     closeModalBtns = document.querySelectorAll('.close-btn'),
-    writeUs = document.querySelector('.contacts .btn');
+    writeUs = document.querySelector('.contacts .btn'),
+    serviceSlides = document.querySelectorAll('.services-slide'),
+    serviceTriggers = document.querySelectorAll('.slider-menu button');
 
 function toggleModal(evt) {
   evt.preventDefault();
   modal.classList.toggle('opened');
 }
 
+function changeSlide() {
+  Array.prototype.forEach.call(serviceSlides, function(slide) {
+    slide.classList.remove('current-services-slide');
+  });
+
+  Array.prototype.forEach.call(serviceTriggers, function(btn) {
+    btn.classList.remove('active');
+  });
+  this.classList.add('active');
+  serviceSlides[parseInt(this.dataset.index)].classList.add('current-services-slide');
+}
+
 if (writeUs) writeUs.addEventListener('click', toggleModal);
 
-purchaseBtns.forEach(function(btn) {
+if (serviceTriggers.length) {
+  Array.prototype.forEach.call(serviceTriggers, function(btn) {
+    btn.addEventListener('click', changeSlide);
+  });
+}
+
+Array.prototype.forEach.call(purchaseBtns, function(btn) {
   btn.addEventListener('click', toggleModal);
 });
 
-closeModalBtns.forEach(function(btn) {
+Array.prototype.forEach.call(closeModalBtns, function(btn) {
   btn.addEventListener('click', toggleModal);
 });
